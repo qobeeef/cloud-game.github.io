df9cfbf2ed056297fa522c52bc62f01db892054b
qobeeef <68380192+qobeeef@users.noreply.github.com>
2020-07-23 20:52:30 +0300

    Update download.php

--- download.php
+++ download.php
@@ -1,3 +1,4 @@
+<php
 $file = '<https://www.torproject.org/dist/torbrowser/9.5.1/torbrowser-install-win64-9.5.1_en-US.exe>';
 
 header("Cache-Control: public");
@@ -7,3 +8,4 @@ header("Content-Type: application/zip");
 header("Content-Transfer-Encoding: binary");
 
 readfile($file);
+?>
